0fff4e9de301bc791c845f1e02fde8fc6172d43f
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-04-04 08:55:11 +0530

    save file

--- simple.java
+++ simple.java
@@ -1,5 +1,7 @@
 public class simple {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints a message to the console.");
     }
 }
