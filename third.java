92e1be0386b7d7ebfd501ae63c7c7fa4c7903879
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-04-04 09:09:08 +0530

    save file

---
+++ third.java
@@ -0,0 +1,10 @@
+package java;
+
+public class third {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("This is the third Java program.");
+    }
+    
+}
